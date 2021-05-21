83b52ee072c4e814fd5f1bba15f329476f2eb489
djembuka <djembuka@gmail.com>
2021-05-21 16:38:26 +0300

    Catalog.colors component

--- markup/components/catalog.colors/script.js
+++ markup/components/catalog.colors/script.js
@@ -2,22 +2,38 @@
   'use strict';
 
   $(function () {
-    document.querySelectorAll('.b-catalog-colors').forEach(function () {});
-
-    if (window.matchMedia('(max-width: 767px)').matches) {
-      new Swiper('.b-catalog-colors__container', {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        freeMode: true,
-        breakpoints: {
-          // when window width is >= 400px
-          400: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-        },
+    document
+      .querySelectorAll('.b-catalog-colors')
+      .forEach(function (colorsBlock) {
+        if (window.matchMedia('(max-width: 767px)').matches) {
+          //gallery
+          new Swiper(
+            colorsBlock.querySelector('.b-catalog-colors__container'),
+            {
+              slidesPerView: 1,
+              spaceBetween: 0,
+              freeMode: true,
+              breakpoints: {
+                // when window width is >= 400px
+                400: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+              },
+            }
+          );
+        } else {
+          //button
+          colorsBlock
+            .querySelector('.btn')
+            .addEventListener('click', function (e) {
+              e.preventDefault();
+              const container = colorsBlock.querySelector(
+                '.b-catalog-colors__container'
+              );
+              colorsBlock.classList.toggle('open');
+            });
+        }
       });
-    } else {
-    }
   });
 })(jQuery);
